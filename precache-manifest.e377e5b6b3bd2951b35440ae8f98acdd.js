7ff1a80440df1639ff771f4a4db34631820ceedc
olakemmy <38473177+olakemmy@users.noreply.github.com>
2020-06-19 10:41:57 +0100

    Add files via upload

---
+++ precache-manifest.e377e5b6b3bd2951b35440ae8f98acdd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf357213cee4a637e6df22b4e200f26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed855fe72b520ebe08aa",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1defce45ff6c75eb4a70",
+    "url": "/static/css/main.8f5ad8af.chunk.css"
+  },
+  {
+    "revision": "ed855fe72b520ebe08aa",
+    "url": "/static/js/2.76b3cefa.chunk.js"
+  },
+  {
+    "revision": "1defce45ff6c75eb4a70",
+    "url": "/static/js/main.db347f66.chunk.js"
+  },
+  {
+    "revision": "48743f4f2bced41a87b1",
+    "url": "/static/js/runtime-main.e7243030.js"
+  },
+  {
+    "revision": "df654e6870cb4875fd6d9c6a8cda2264",
+    "url": "/static/media/Blog.df654e68.jpg"
+  },
+  {
+    "revision": "d7c5ac4e68a5ea6ba3335bc40933ac8c",
+    "url": "/static/media/Jaytees.d7c5ac4e.jpg"
+  },
+  {
+    "revision": "17fc66211f9c0e1606dd92c62006d8c2",
+    "url": "/static/media/Ola.17fc6621.jpeg"
+  },
+  {
+    "revision": "2e25a4e99ef16cad6f69b8adefa570c1",
+    "url": "/static/media/Web.2e25a4e9.png"
+  },
+  {
+    "revision": "aeb40d45ab27b1b5c68d6121df1e8939",
+    "url": "/static/media/facilitator.aeb40d45.png"
+  },
+  {
+    "revision": "527aad4643d170519be3e8b3727bd632",
+    "url": "/static/media/social.527aad46.png"
+  },
+  {
+    "revision": "2977fb7fe34360924515151934168857",
+    "url": "/static/media/uiux.2977fb7f.png"
+  }
+]);
